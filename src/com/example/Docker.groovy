bf7a7e743aef8bdfc9d8052c7033ade6cc744520
admin <rik215@live.com>
2026-09-21 23:30:00 -0400

    added function call from src package

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,19 @@
+#!/user/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker (script) {
+        this.script = script
+    }
+    def buildDockerImage(String imageName) {
+        script.echo "building docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'rik215', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
