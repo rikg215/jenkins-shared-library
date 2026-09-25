9ecede131c6fb4e2b4f2aa8b686d3faef352fc48
rikg215 <rikg215@gmail.com>
2026-09-25 10:51:20 -0400

    Update Docker.groovy
    updated credential id ref

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -15,7 +15,7 @@ class Docker implements Serializable {
 
 
     def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'rik215', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub_pat', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
         }
     }
